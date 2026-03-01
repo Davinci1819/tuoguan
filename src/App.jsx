ff44903b10ade2ec2d0b48679b872378c69d3105
Davinci1819 <diogenes323324@outlook.com>
2026-03-01 21:01:52 +0800

    feat: AI 生成文件 version 11

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// @ts-ignore;
+import React from 'react';
+
+import HomeworkRegister from './pages/homework_register.jsx';
+import './index.css';
+function App() {
+  return <div className="min-h-screen bg-gray-50">
+      <HomeworkRegister />
+    </div>;
+}
+export default App;
